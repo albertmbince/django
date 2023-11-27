eaa89025d2b8b45524f02d4aff639371f020752b
Albert M Bince <133676636+albertmbince@users.noreply.github.com>
2023-11-27 18:01:07 +0530

    Add files via upload

--- project3albert/project3/project3/settings.py
+++ project3albert/project3/project3/settings.py
@@ -29,6 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+LOGIN_URL='user_login'
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL="user_login"
+LOGOUT_URL='user_logout'
+
+
 
 # Application definition
 
